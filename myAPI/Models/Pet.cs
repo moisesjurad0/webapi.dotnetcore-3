d87cde3a9c8c0f4685e0e66798bae088dddc7937
Moisés Jurado <moises003@gmail.com>
2023-06-08 01:05:42 -0500

    code first and docker

---
+++ myAPI/Models/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace myAPI.Models
+{
+    //[Table("Pets")]
+    public class Pet
+
+    {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { set; get; }
+
+        [Required]
+        public string? Name { set; get; }
+
+        public int Paws { set; get; }
+
+    }
+}
--- myAPI/Models/myContext.cs
+++ myAPI/Models/myContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
+
 namespace myAPI.Models
 {
-    public class myContext : DbContext 
+    public class myContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("your-postgresql-connection-string");
+            //optionsBuilder.UseNpgsql("your-postgresql-connection-string");
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=mydatabase;User Id=postgres;Password=password");
         }
-
+        // Define DbSet properties for your entities
+        public DbSet<Pet> Pets { get; set; }
     }
 }
